14ae6ee1bab9f48b4fb4533601278d29ce3f1dd0
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-10-26 22:20:48 +0300

    added some algos

---
+++ TwoCrystalBalls.ts
@@ -0,0 +1,10 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+}
