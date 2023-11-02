0563c348754e434c8bffa9c19267de315871c909
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-11-02 23:23:42 +0200

    added remove functionality to doublylinkedlist

--- DoublyLinkedList.ts
+++ DoublyLinkedList.ts
@@ -1,71 +1,106 @@
 type Node<T> = {
-    value: T;
-    prev?: Node<T>;
-    next?: Node<T>;
+  value: T;
+  prev?: Node<T>;
+  next?: Node<T>;
 };
 
 export default class DoublyLinkedList<T> {
-    public length: number;
-    private head?: Node<T>;
-    private tail?: Node<T>;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    constructor() {
-        this.length = 0;
-        this.head = undefined;
-        this.tail = undefined;
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+    this.tail = undefined;
+  }
+
+  prepend(item: T): void {
+    const node = { value: item } as Node<T>;
+
+    this.length++;
+    if (!this.head) {
+      this.head = this.tail = node;
+      return;
     }
 
-    prepend(item: T): void {
-        const node = { value: item } as Node<T>;
-
-        this.length++;
-        if (!this.head) {
-            this.head = this.tail = node;
-            return;
-        }
-
-        node.next = this.head;
-        this.head.prev = node;
-        this.head = node;
+    node.next = this.head;
+    this.head.prev = node;
+    this.head = node;
+  }
+  insertAt(item: T, idx: number): void {
+    if (idx > this.length) {
+      throw new Error("oh no");
+    } else if (idx === this.length) {
+      this.prepend(item);
+      return;
+    } else if (idx === 0) {
+      this.append(item);
+      return;
     }
-    insertAt(item: T, idx: number): void {
-        if (idx > this.length) {
-            throw new Error("oh no");
-        } else if (idx === this.length) {
-            this.prepend(item);
-            return;
-        } else if (idx === 0) {
-            this.append(item);
-            return;
-        }
-        let curr = this.head;
-        for (let i = 0; curr && i < idx; ++i) {
-            curr = curr.next;
-        }
-
-        this.length++;
-        curr = curr as Node<T>;
-        const node = { value: item } as Node<T>;
-
-        node.next = curr;
-        node.prev = curr.prev;
-        curr.prev = node;
-        if (curr.prev) {
-            curr.prev.next = curr;
-        }
+    let curr = this.head;
+    for (let i = 0; curr && i < idx; ++i) {
+      curr = curr.next;
     }
-    append(item: T): void {
-        this.length++;
-        const node = { value: item } as Node<T>;
-        if (!this.tail) {
-            this.head = this.tail = node;
-            return;
-        }
-        node.prev = this.tail;
-        this.tail.next = node;
-        this.tail = node;
+
+    this.length++;
+    curr = curr as Node<T>;
+    const node = { value: item } as Node<T>;
+
+    node.next = curr;
+    node.prev = curr.prev;
+    curr.prev = node;
+    if (curr.prev) {
+      curr.prev.next = curr;
     }
-    remove(item: T): T | undefined {}
-    get(idx: number): T | undefined {}
-    removeAt(idx: number): T | undefined {}
+  }
+  append(item: T): void {
+    this.length++;
+    const node = { value: item } as Node<T>;
+    if (!this.tail) {
+      this.head = this.tail = node;
+      return;
+    }
+    node.prev = this.tail;
+    this.tail.next = node;
+    this.tail = node;
+  }
+  remove(item: T): T | undefined {
+    let curr = this.head;
+    for (let i = 0; curr && i < this.length; ++i) {
+      if (curr.value === item) {
+        break;
+      }
+      curr = curr.next;
+    }
+    if (!curr) {
+      return undefined;
+    }
+
+    this.length--;
+
+    if (this.length === 0) {
+      const out = this.head?.value;
+      this.head = this.tail = undefined;
+      return out;
+    }
+
+    if (curr.prev) {
+      curr.prev = curr.next;
+    }
+    if (curr.next) {
+      curr.next = curr.prev;
+    }
+
+    if (curr === this.head) {
+      this.head = curr.next;
+    }
+    if (curr === this.tail) {
+      this.tail = curr.prev;
+    }
+    curr.prev = curr.next = undefined;
+    return curr.value;
+  }
+  get(idx: number): T | undefined {}
+  removeAt(idx: number): T | undefined {}
 }
