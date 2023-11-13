6d64825d563c22d22d94a96d39fcccc2e70a9d6a
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-11-13 19:42:35 +0200

    added breadth first search

--- BTInOrder.ts
+++ BTInOrder.ts
@@ -1,3 +1,21 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  //binary tree in order
+  if (!curr) {
+    return path;
+  }
 
+  //recurse
+  //pre
+  walk(curr.left, path);
+
+  //recurse
+  path.push(curr.value);
+  walk(curr.right, path);
+
+  //post
+  return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
